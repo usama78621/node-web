7129dd67cb7968954e109e16fb9e8ad120794274
usama78621 <92777217+usama78621@users.noreply.github.com>
2022-07-30 12:29:38 +0500

    api keys update

--- src/uiltes/forcasts.js
+++ src/uiltes/forcasts.js
@@ -1,16 +1,16 @@
 const request = require('postman-request');
-const forcast = (longitude,latitude,callback)=>{
-    const url = 'http://api.weatherstack.com/current?access_key=739bd0bd9292920f6c5f07fe9598ba30&query='+ latitude +','+ longitude +'&units=f'
-   
-request({url, json:true},(error,{body})=>{
-      if(error){
-        callback('unable to connect your location!',undefined)
-      } else if(body.error){
-        callback('unable to fined the location,You Search?',undefined)
-      } else{
-       callback( undefined,'Now '+ body.current.weather_descriptions+ ' its current Temperature ' +body.current.temperature +' Feel like this ' +body.current.feelslike )
-       }
-   });
-  }
+const forcast = (longitude, latitude, callback) => {
+  const url = 'http://api.weatherstack.com/current?access_key=4f7eaa7048c55dbe87b28c8b32edc82e&query=' + latitude + ',' + longitude + '&units=f'
 
-  module.exports = forcast;
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('unable to connect your location!', undefined)
+    } else if (body.error) {
+      callback('unable to fined the location,You Search?', undefined)
+    } else {
+      callback(undefined, 'Now ' + body.current.weather_descriptions + ' its current Temperature ' + body.current.temperature + ' Feel like this ' + body.current.feelslike)
+    }
+  });
+}
+
+module.exports = forcast;
